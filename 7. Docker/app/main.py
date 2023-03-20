ac7771c9a088576e1cd22b17a36305c3a7790d84
Isaac Benavides <isaac_al_saca@hotmail.com>
2023-03-20 14:07:24 -0600

    Agregado Docker Compose

--- 7. Docker/app/main.py
+++ 7. Docker/app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 
 mydb = mysql.connector.connect(
-  host="localhost",
+  host="lyrics-db",
   user="root",
   password="cisco123",
   database="lyrics"
@@ -18,4 +18,4 @@ mycursor = mydb.cursor(buffered=True)
 async def root():
     mycursor.execute("SELECT * FROM lyrics ORDER BY RAND()")
     response = mycursor.fetchone()
-    return "Bonjour"
+    return response
--- 7. Docker/readme.md
+++ 7. Docker/readme.md
@@ -1,1 +1,15 @@
-docker network create
+docker network create lyrics-network
+docker build -t lyrics-api:1.0 .
+
+docker run --name lyrics-db \
+    -dp 3306:3306 \
+    -e MYSQL_ROOT_PASSWORD=cisco123 \
+    -v mysql:/var/lib/mysql \
+    --network lyrics-network \
+    mysql:8
+
+docker run --name lyrics-api \
+    --network lyrics-network \
+    -p 80:80 \
+    lyrics-api:1.0
+
